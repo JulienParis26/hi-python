fb0816477c409dc61e7c208b32ace4ff9a6cc752
JulienParis26 <71380678+JulienParis26@users.noreply.github.com>
2021-09-23 11:28:19 +0200

    Add 02 Guessing Game

---
+++ ex02_guessing.py
@@ -0,0 +1,22 @@
+# Import the module
+import json
+
+import random
+
+n = random.randint(1, 50)
+
+number = int(input("Enter an integer from 1 to 50: "))
+while n != "number":
+   
+    if number < n:
+        print("It is more")
+        number = int(input("Enter an number from 1 to 50: "))
+
+    elif number > n:
+        print("It is less")
+        number = int(input("Enter an number from 1 to 50: "))
+
+    else:
+        print("The account is good!")
+        break
+    
